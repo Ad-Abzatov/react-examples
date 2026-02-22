7cff10afe6fad2d851b222a370508f4d92a58295
seyar_ad <ad-abzatov.seyar@mail.ru>
2026-02-22 22:41:04 +0300

    usestate

--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -2,11 +2,17 @@ import { useState } from "react"
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
+  const [array, setArray] = useState<number[]>([]);
 
   return (
     <div>
-      {/* <button>{count}</button> */}
       Хуки
+      <p>useState</p>
+      {count}
+      <button onClick={() => setCount(Math.round(Math.random()*100))}>Рандом</button>
+      <br/>
+      {array.join(', ')}
+      <button onClick={() => setArray(item => [...item, Math.round(Math.random()*100)])}>Добавить массив</button>
     </div>
   )
 }
