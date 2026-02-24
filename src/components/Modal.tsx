143a3fe4c2624816015ffb02eee68b1b276e21e9
seyar_ad <ad-abzatov.seyar@mail.ru>
2026-02-24 22:25:50 +0300

    style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,11 +9,13 @@ const Modal: FC<ModalProps> = ({isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div>
-      <h2>Всплывающая форма</h2>
-      <form>
-        <button type="button" onClick={onClose}>Закрыть</button>
-      </form>
+    <div className="overlay">
+      <div className="modal">
+        <h2>Всплывающая форма</h2>
+        <form>
+          <button type="button" onClick={onClose}>Закрыть</button>
+        </form>
+      </div>
     </div>
   )
 }
